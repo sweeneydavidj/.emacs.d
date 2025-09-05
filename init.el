54cb731f87567b13d5958757b532791cc7dfff76
David Sweeney <sweeneydavidj@gmail.com>
2025-09-05 13:54:18 +0200

    tree-sitter language grammars

--- init.el
+++ init.el
@@ -355,5 +355,32 @@
   :config
   (global-dot-mode t))
 
+;; See https://github.com/mickeynp/combobulate
+(defun dsw-setup-install-grammars ()
+  "Install Tree-sitter grammars if they are absent."
+  (interactive)
+  (dolist (grammar
+           '(
+             (elixir . ("https://github.com/elixir-lang/tree-sitter-elixir" "v0.3.4"))
+             (heex . ("https://github.com/phoenixframework/tree-sitter-heex" "v0.8.0"))
+             ;; (css . ("https://github.com/tree-sitter/tree-sitter-css" "v0.20.0"))
+             ;; (go . ("https://github.com/tree-sitter/tree-sitter-go" "v0.20.0"))
+             ;; (html . ("https://github.com/tree-sitter/tree-sitter-html" "v0.20.1"))
+             ;; (javascript . ("https://github.com/tree-sitter/tree-sitter-javascript" "v0.20.1" "src"))
+             ;; (json . ("https://github.com/tree-sitter/tree-sitter-json" "v0.20.2"))
+             ;; (markdown . ("https://github.com/ikatyang/tree-sitter-markdown" "v0.7.1"))
+             ;; (python . ("https://github.com/tree-sitter/tree-sitter-python" "v0.20.4"))
+             ;; (rust . ("https://github.com/tree-sitter/tree-sitter-rust" "v0.21.2"))
+             ;; (toml . ("https://github.com/tree-sitter/tree-sitter-toml" "v0.5.1"))
+             ;; (tsx . ("https://github.com/tree-sitter/tree-sitter-typescript" "v0.20.3" "tsx/src"))
+             ;; (typescript . ("https://github.com/tree-sitter/tree-sitter-typescript" "v0.20.3" "typescript/src"))
+             ;; (yaml . ("https://github.com/ikatyang/tree-sitter-yaml" "v0.5.0"))
+             ))
+    (add-to-list 'treesit-language-source-alist grammar)
+    ;; Only install `grammar' if we don't already have it
+    ;; installed. However, if you want to *update* a grammar then
+    ;; this obviously prevents that from happening.
+    (unless (treesit-language-available-p (car grammar))
+      (treesit-install-language-grammar (car grammar)))))
 
 ;;; init.el ends here
