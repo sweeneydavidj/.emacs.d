e35fc9b4bec92756675adbafba40c18c8296328f
David Sweeney <sweeneydavidj@gmail.com>
2025-12-12 16:57:30 +0100

    reorder

--- init.el
+++ init.el
@@ -80,20 +80,12 @@
 (setq savehist-additional-variables '(register-alist))
 (savehist-mode 1)
 
-(use-package diminish)
-
-;; (use-package doom-themes
-;;   :config
-;;   ;; Global settings (defaults)
-;;   (setq doom-themes-enable-bold t    ; if nil, bold is universally disabled
-;;         doom-themes-enable-italic t) ; if nil, italics is universally disabled
-;;   (load-theme 'doom-one t)
-;;   ;; Enable flashing mode-line on errors
-;;   (doom-themes-visual-bell-config)
-;;   ;; Corrects (and improves) org-mode's native fontification.
-;;   (doom-themes-org-config))
+;; https://laurencewarne.github.io/emacs/programming/2022/12/26/exploring-proced.html
+(setq proced-enable-color-flag t)
 
 (load-theme 'modus-vivendi-tinted)
+
+(use-package diminish)
 
 (use-package solaire-mode
   :config
@@ -418,8 +410,5 @@
     ;; this obviously prevents that from happening.
     (unless (treesit-language-available-p (car grammar))
       (treesit-install-language-grammar (car grammar)))))
-
-;; https://laurencewarne.github.io/emacs/programming/2022/12/26/exploring-proced.html
-(setq proced-enable-color-flag t)
 
 ;;; init.el ends here
